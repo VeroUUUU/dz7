27512ac85202504bed74a44b6f51926af364a041
veronika <verouuu>
2022-05-14 11:01:39 +0300

    Initial commit

--- src/dz6/fiboNacci.java
+++ src/dz6/fiboNacci.java
@@ -1,2 +1,16 @@
-package dz6;public class fiboNacci {
+package dz6;
+
+public class fiboNacci {
+    public static void main(String[] args) {
+        int fib0 = 1;
+        int fib1 = 1;
+        int fib2;
+        System.out.print(fib0 + " " + fib1 +" ");
+        for (int i=3; i<=11; i++){
+            fib2 = fib0 + fib1;
+            System.out.printf(fib2 + " ");
+            fib0 = fib1;
+            fib1 = fib2;
+        }
+    }
 }
